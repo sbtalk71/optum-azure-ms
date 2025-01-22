a8543b3b0c3f15b65dd10ce674b14e65e68b7f75
sbtalk71 <sbtalk@yahoo.com>
2025-01-22 21:33:11 +0530

    security config updated

--- api-gateway/src/main/java/com/demo/spring/SecurityConfig.java
+++ api-gateway/src/main/java/com/demo/spring/SecurityConfig.java
@@ -1,24 +1,29 @@
 package com.demo.spring;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain config(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(sec->sec.requestMatchers("/orders/**").hasRole("USER")
-				.requestMatchers("/inventory/**").hasRole("ADMIN").anyRequest().permitAll());
+	public SecurityWebFilterChain  config(ServerHttpSecurity http) throws Exception {
+		http.authorizeExchange(sec->sec.pathMatchers("/orders/**").hasRole("USER")
+				.pathMatchers("/inventory/**").hasRole("ADMIN").anyExchange().permitAll());
 		
 		http.httpBasic(Customizer.withDefaults()).csrf(Customizer.withDefaults());
 		return http.build();
@@ -31,11 +36,12 @@ public class SecurityConfig {
 	
 	
 	@Bean
-	public UserDetailsService userDetailsService() throws Exception{
-		InMemoryUserDetailsManager manager= new InMemoryUserDetailsManager();
-		manager.createUser(User.withUsername("shantanu").password(passwordEncoder().encode("welcome1")).roles("ADMIN").build()); 
-		manager.createUser(User.withUsername("pavan").password(passwordEncoder().encode("welcome1")).roles("USER").build()); 
-		manager.createUser(User.withUsername("ranga").password(passwordEncoder().encode("welcome1")).roles("USER").build()); 
+	public MapReactiveUserDetailsService  userDetailsService() throws Exception{
+		
+		UserDetails u1=(User.withUsername("shantanu").password(passwordEncoder().encode("welcome1")).roles("ADMIN").build()); 
+		UserDetails u2=(User.withUsername("pavan").password(passwordEncoder().encode("welcome1")).roles("USER").build()); 
+		UserDetails u3=(User.withUsername("ranga").password(passwordEncoder().encode("welcome1")).roles("USER").build()); 
+		MapReactiveUserDetailsService manager= new MapReactiveUserDetailsService(Arrays.asList(u1,u2,u3));
 		return manager;
 	}
 }
